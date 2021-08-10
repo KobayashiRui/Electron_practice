17af82b002e0ef40c1a4859a32aac0e80b4de325
Kobayashi Rui <roboroborx782@gmail.com>
2021-08-10 23:51:04 +0900

    update

---
+++ ipc-communicate-unsafe-app/main.js
@@ -0,0 +1,35 @@
+const { app, ipcMain, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  // macOS を除き、全ウインドウが閉じられたときに終了します。
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+
+ipcMain.handle("hello", (event) => {
+   return "HelloWorld"
+})
