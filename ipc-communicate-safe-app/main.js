670f56b6b5ebea52a66e752a58da370fe72c9904
Kobayashi Rui <roboroborx782@gmail.com>
2021-08-13 21:37:37 +0900

    update

--- ipc-communicate-safe-app/main.js
+++ ipc-communicate-safe-app/main.js
@@ -1,11 +1,12 @@
 const { app, ipcMain, BrowserWindow } = require('electron')
+const path = require("path");
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: { 
-      preload: __dirname + '/preload.js' //preloadするjs指定
+      preload: path.join(__dirname + '/preload.js') //preloadするjs指定
     }
   })
 
